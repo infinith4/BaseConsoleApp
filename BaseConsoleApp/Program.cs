1a623aef4a761836e9faf1bfd0963e64253ccd25
infinith4 <infinith4@gmail.com>
2018-11-03 15:19:53 +0900

    use MarkdownNet

---
+++ BaseConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var article = new MarkdownNet.Models.ArticleModel();
+            article.ArticleName = "評価仕様書兼成績書";
+            article.Title = "タイトル";
+            article.SubTitle = "サブタイトル";
+            article.Content = "ddddd,ddddd,dddddd,dddddf,,,ddddd,\r\nddddd,ddddd,dddddd,dddddf,,,ddddd,";
+
+            MarkdownNet.GenerateMarkdown.CreateMarkdown(article);
+        }
+    }
+}
